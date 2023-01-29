efbdb56f1bc48c2d13694ae3dedcb4e79ef370f3
Nigel van Keulen <Nigelvk@itextra.nl>
2023-01-29 15:27:42 +0100

    Comments

--- route.go
+++ route.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Nigel2392/routevars"
 )
 
+// Route is a single route in the router
 type Route struct {
 	Method      string
 	Path        string
@@ -14,6 +15,7 @@ type Route struct {
 	children    []*Route
 }
 
+// HandleFunc registers a new route with the given path and method.
 func (r *Route) HandleFunc(method, path string, handler HandleFunc) Registrar {
 	path = r.Path + path
 	var child = &Route{
@@ -26,42 +28,52 @@ func (r *Route) HandleFunc(method, path string, handler HandleFunc) Registrar {
 	return child
 }
 
+// Handle is a convenience method that wraps the http.Handler in a HandleFunc
 func (r *Route) Handle(method, path string, handler http.Handler) Registrar {
 	return r.HandleFunc(method, path, HTTPWrapper(handler.ServeHTTP))
 }
 
+// Put registers a new route with the given path and method.
 func (r *Route) Put(path string, handler HandleFunc) Registrar {
 	return r.HandleFunc("PUT", path, handler)
 }
 
+// Post registers a new route with the given path and method.
 func (r *Route) Post(path string, handler HandleFunc) Registrar {
 	return r.HandleFunc("POST", path, handler)
 }
 
+// Get registers a new route with the given path and method.
 func (r *Route) Get(path string, handler HandleFunc) Registrar {
 	return r.HandleFunc("GET", path, handler)
 }
 
+// Delete registers a new route with the given path and method.
 func (r *Route) Delete(path string, handler HandleFunc) Registrar {
 	return r.HandleFunc("DELETE", path, handler)
 }
 
+// Patch registers a new route with the given path and method.
 func (r *Route) Patch(path string, handler HandleFunc) Registrar {
 	return r.HandleFunc("PATCH", path, handler)
 }
 
+// Options registers a new route with the given path and method.
 func (r *Route) Options(path string, handler HandleFunc) Registrar {
 	return r.HandleFunc("OPTIONS", path, handler)
 }
 
+// Head registers a new route with the given path and method.
 func (r *Route) Head(path string, handler HandleFunc) Registrar {
 	return r.HandleFunc("HEAD", path, handler)
 }
 
+// Register a route for all methods
 func (r *Route) Any(path string, handler HandleFunc) Registrar {
 	return r.HandleFunc(ALL, path, handler)
 }
 
+// Group creates a new group of routes
 func (r *Route) Group(path string, middlewares ...func(Handler) Handler) Registrar {
 	var route = &Route{Path: path}
 	route.middleware = append(r.middleware, middlewares...)
@@ -69,6 +81,7 @@ func (r *Route) Group(path string, middlewares ...func(Handler) Handler) Registr
 	return route
 }
 
+// Match checks if the given path matches the route
 func (r *Route) Match(method, path string) (bool, *Route, Vars) {
 	if r.Method != ALL {
 		if r.Method != method && r.HandlerFunc != nil {
@@ -87,6 +100,7 @@ func (r *Route) Match(method, path string) (bool, *Route, Vars) {
 	return false, nil, nil
 }
 
+// Use adds middleware to the route
 func (r *Route) Use(middlewares ...func(Handler) Handler) {
 	r.middleware = append(r.middleware, middlewares...)
 }
--- router.go
+++ router.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 )
 
+// HTTP Methods
 const (
 	GET     = "GET"
 	POST    = "POST"
@@ -12,19 +13,26 @@ const (
 	PATCH   = "PATCH"
 	OPTIONS = "OPTIONS"
 	HEAD    = "HEAD"
-	ALL     = "ALL"
+
+	// Special method for matching all methods
+	ALL = "ALL"
 )
 
+// HandleFunc is the function that is called when a route is matched
 type HandleFunc func(Vars, http.ResponseWriter, *http.Request)
 
+// Wrapper function for HandleFunc to make it compatible with http.Handler
 type HandleFuncWrapper struct {
 	F func(Vars, http.ResponseWriter, *http.Request)
 }
 
+// ServeHTTP implements the Handler interface
 func (h HandleFuncWrapper) ServeHTTP(vars Vars, w http.ResponseWriter, req *http.Request) {
 	h.F(vars, w, req)
 }
 
+// Registrar is the main interface for registering routes
+// Both the router and the route struct implement this interface
 type Registrar interface {
 	Put(path string, handler HandleFunc) Registrar
 	Post(path string, handler HandleFunc) Registrar
@@ -35,22 +43,28 @@ type Registrar interface {
 	Head(path string, handler HandleFunc) Registrar
 	HandleFunc(method, path string, handler HandleFunc) Registrar
 	Handle(method, path string, handler http.Handler) Registrar
+	Any(path string, handler HandleFunc) Registrar
 	Use(middlewares ...func(Handler) Handler)
 	Group(path string, middlewares ...func(Handler) Handler) Registrar
 }
 
+// Handler is the interface that wraps the ServeHTTP method.
 type Handler interface {
 	ServeHTTP(Vars, http.ResponseWriter, *http.Request)
 }
 
+// Variable map passed to the route.
 type Vars map[string]string
 
+// Wrapper function for http.Handler to make it compatible with HandleFunc
 func HTTPWrapper(handler func(http.ResponseWriter, *http.Request)) HandleFunc {
 	return func(vars Vars, w http.ResponseWriter, req *http.Request) {
 		handler(w, req)
 	}
 }
 
+// Router is the main router struct
+// It takes care of dispatching requests to the correct route
 type Router struct {
 	routes     []*Route
 	middleware []func(Handler) Handler
@@ -58,53 +72,70 @@ type Router struct {
 	skipTrailingSlash bool
 }
 
+// NewRouter creates a new router
 func NewRouter(SkipTrailingSlash bool) *Router {
 	var r = &Router{routes: make([]*Route, 0), middleware: make([]func(Handler) Handler, 0), skipTrailingSlash: SkipTrailingSlash}
 	return r
 }
 
+// HandleFunc registers a new route with the given path and method.
 func (r *Router) HandleFunc(method, path string, handler HandleFunc) Registrar {
 	var route = &Route{Method: method, Path: path, HandlerFunc: handler}
 	r.routes = append(r.routes, route)
 	return route
 }
 
+// Handle is a convenience method that wraps the http.Handler in a HandleFunc
 func (r *Router) Handle(method, path string, handler http.Handler) Registrar {
 	return r.HandleFunc(method, path, HTTPWrapper(handler.ServeHTTP))
 }
 
+// Put registers a new route with the given path and method.
 func (r *Router) Put(path string, handler HandleFunc) Registrar {
 	return r.HandleFunc("PUT", path, handler)
 }
 
+// Post registers a new route with the given path and method.
 func (r *Router) Post(path string, handler HandleFunc) Registrar {
 	return r.HandleFunc("POST", path, handler)
 }
 
+// Get registers a new route with the given path and method.
 func (r *Router) Get(path string, handler HandleFunc) Registrar {
 	return r.HandleFunc("GET", path, handler)
 }
 
+// Delete registers a new route with the given path and method.
 func (r *Router) Delete(path string, handler HandleFunc) Registrar {
 	return r.HandleFunc("DELETE", path, handler)
 }
 
+// Patch registers a new route with the given path and method.
 func (r *Router) Patch(path string, handler HandleFunc) Registrar {
 	return r.HandleFunc("PATCH", path, handler)
 }
 
+// Options registers a new route with the given path and method.
 func (r *Router) Options(path string, handler HandleFunc) Registrar {
 	return r.HandleFunc("OPTIONS", path, handler)
 }
 
+// Head registers a new route with the given path and method.
 func (r *Router) Head(path string, handler HandleFunc) Registrar {
 	return r.HandleFunc("HEAD", path, handler)
 }
 
+// Register a route for all methods
+func (r *Router) Any(path string, handler HandleFunc) Registrar {
+	return r.HandleFunc(ALL, path, handler)
+}
+
+// Use adds middleware to the router.
 func (r *Router) Use(middlewares ...func(Handler) Handler) {
 	r.middleware = append(r.middleware, middlewares...)
 }
 
+// Group creates new children to a route.
 func (r *Router) Group(path string, middlewares ...func(Handler) Handler) Registrar {
 	var route = &Route{Path: path}
 	route.middleware = append(r.middleware, middlewares...)
@@ -112,6 +143,8 @@ func (r *Router) Group(path string, middlewares ...func(Handler) Handler) Regist
 	return route
 }
 
+// ServeHTTP dispatches the request to the handler whose
+// pattern matches the request URL.
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
 	if r.skipTrailingSlash && len(req.URL.Path) > 1 && req.URL.Path[len(req.URL.Path)-1] == '/' {
