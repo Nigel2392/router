78638ac70bbd89bf44f50c894526ee6396d1e13c
Nigel van Keulen <Nigelvk@itextra.nl>
2023-02-22 13:59:37 +0100

    Update

--- v2/middleware/auth.go
+++ v2/middleware/auth.go
@@ -9,6 +9,7 @@ func AddUserMiddleware(f func(*request.Request) request.User) router.Middleware 
 	return func(next router.Handler) router.Handler {
 		return router.ToHandler(func(r *request.Request) {
 			r.User = f(r)
+			r.Data.User = r.User
 			next.ServeHTTP(r)
 		})
 	}
